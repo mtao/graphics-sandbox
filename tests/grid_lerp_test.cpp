7abb10a3aad89b6bed6281c4f3b505d2ab6aae9d
Michael Tao <mtao@dgp.toronto.edu>
2014-05-18 10:13:04 -0500

    lerp tests seem alright

--- tests/grid_lerp_test.cpp
+++ tests/grid_lerp_test.cpp
@@ -1,8 +1,8 @@
 #include "grid/grid.h"
 #include <iostream>
 
-int main(int argc, char * argv[]) {
 
+int test1() {
     mtao::Grid<float,1> g1;
     mtao::Vec1i N;
     N(0) = 2;
@@ -13,5 +13,30 @@ int main(int argc, char * argv[]) {
     mtao::Vec1f center;
     center[0] = 0.5;
     std::cout << c.lerp(center) << std::endl;
-    
+    return std::abs(c.lerp(center)-2) > 1e-5;
+}
+int test2() {
+    mtao::Grid<float,2> g;
+    mtao::Vec2i N;
+    N(0) = 2;
+    N(1) = 2;
+    g.reshape(N);
+    g(0,0) = 1;
+    g(1,0) = 2;
+    g(0,1) = 3;
+    g(1,1) = 4;
+    mtao::lerp::Cube<float,2> c(g,mtao::Vec2i::Zero());
+    mtao::Vec2f center;
+    center[0] = 0.5;
+    center[1] = 0.5;
+    std::cout << c.lerp(center) << std::endl;
+    return std::abs(c.lerp(center)-2.5) > 1e-5;
+}
+int main(int argc, char * argv[]) {
+    int ret = 0;
+    ret |= test1();
+    std::cout << ret << std::endl;
+    ret |= test2();
+    std::cout << ret << std::endl;
+    return ret;
 }
